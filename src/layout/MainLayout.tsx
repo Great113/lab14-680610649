218266e081842c25704a5537feb7bad6f802c592
Korrawit Yawichai <greater2318@gmail.com>
2026-09-11 18:19:41 +0700

    ห

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -20,7 +20,7 @@ export default function MainLayout() {
           </main>
 
           {/* Footer ด้านล่าง */}
-          <Footer year="2026" fullName="ชื่อ-สกุล" studentId="รหัสนักศึกษา" />
+          <Footer year="2026" fullName="กรวิชญ์ ยาวิชัย" studentId="680610649" />
         </div>
       </div>
     </div>
